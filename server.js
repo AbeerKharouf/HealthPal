f55f2d264adbb5ff255a7e8e14debd1ecda693a4
ramaabughdeeb <s12112712@stu.najah.edu>
2025-12-05 21:52:05 +0200

    Book appoointments online

--- server.js
+++ server.js
@@ -6,31 +6,28 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 
-
 app.use(cors());
 app.use(express.json());
 
-// اختبار الاتصال
 app.get("/", (req, res) => {
   res.send("HealthPal API is running...");
 });
 
-// مثال: جلب كل المستخدمين
 app.get("/users", (req, res) => {
   db.query("SELECT * FROM users", (err, result) => {
     if (err) return res.json(err);
     res.json(result);
   });
 });
+const appointmentsRouter = require("./routes/appointments");
+app.use("/appointments", appointmentsRouter);
 
-// مسارات المستخدم
+
 app.use("/api", userRoutes);
 
-console.log("🔥 appointments ROUTE MOUNTED");
 
-const doctorRouter = require("./routes/doctor"); // أو doctors حسب اسم الملف
+const doctorRouter = require("./routes/doctor");
 app.use("/doctor", doctorRouter);
-
 
 
 app.listen(5000, () => {
